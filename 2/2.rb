e52289e840faf86b371dac58bf4d2a7283e3c961
Cory Forsyth <cory.forsyth@gmail.com>
2010-08-22 17:52:52 -0400

    Simplify using vert_row and horiz_row, and add a description of the sections

--- 2/2.rb
+++ 2/2.rb
@@ -1,4 +1,21 @@
 class LCD
+  # Sections are labeled top-to-bottom, left-to-right, like so:
+  #            
+  #     --0-- 
+  #    |     |
+  #    |     |
+  #    1     2
+  #    |     |
+  #    |     |
+  #     --3-- 
+  #    |     |
+  #    |     |
+  #    4     5
+  #    |     |
+  #    |     |
+  #     --6--
+  #
+
   NUMBER_SECTIONS = [
     [true,  true,  true, false, true,  true,  true],
     [false, false, true, false, false, true,  false],
@@ -28,61 +45,66 @@ class LCD
     output = []
     
     output << row0(@numbers)
-    
     @size.times do
       output << row1(@numbers)
     end
-    
     output << row2(@numbers)
-
     @size.times do
       output << row3(@numbers)
     end
-    
     output << row4(@numbers)
-    
     output.join("\n")
   end
 
-  def row0(numbers)
-    numbers.map do |number|
-      SEPARATOR + (str_for_section(number, 0) * @size) + SEPARATOR
-    end.join(SEPARATOR)
-  end
-  
-  def str_type_for_section(section)
-    [0,3,6].include?(section) ? HORIZ_SEGMENT : VERT_SEGMENT
-  end
-  
   def lit_for_section(number, section)
     NUMBER_SECTIONS[number][section]
   end
-  
+
+  def str_type_for_section(section)
+    [0,3,6].include?(section) ? HORIZ_SEGMENT : VERT_SEGMENT
+  end
+
   def str_for_section(number, section)
    lit_for_section(number, section) ? str_type_for_section(section) : SEPARATOR
   end
 
+  def horiz_row(number, section)
+    SEPARATOR + (str_for_section(number, section) * @size) + SEPARATOR
+  end
+  
+  def vert_row(number, section_left, section_right)
+    str_for_section(number, section_left) + 
+      (SEPARATOR * @size) + 
+      str_for_section(number, section_right)
+  end
+
+  def row0(numbers)
+    numbers.map do |number|
+      horiz_row(number, 0)
+    end.join(SEPARATOR)
+  end
+  
   def row1(numbers)
     numbers.map do |number|
-      str_for_section(number, 1) + (SEPARATOR * @size) + str_for_section(number, 2)
+      vert_row(number, 1, 2)
     end.join(SEPARATOR)
   end
   
   def row2(numbers)
     numbers.map do |number|
-      SEPARATOR + str_for_section(number, 3) * @size + SEPARATOR
+      horiz_row(number, 3)
     end.join(SEPARATOR)
   end
   
   def row3(numbers)
     numbers.map do |number|
-      str_for_section(number, 4) + (SEPARATOR * @size) + str_for_section(number, 5)
+      vert_row(number, 4, 5)
     end.join(SEPARATOR)
   end
   
   def row4(numbers)
     numbers.map do |number|
-      SEPARATOR + str_for_section(number, 6) * @size + SEPARATOR
+      horiz_row(number, 6)
     end.join(SEPARATOR)
   end
 end
