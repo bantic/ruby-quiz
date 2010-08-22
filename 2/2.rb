66c4c954053ac7d52143f63a0af12cd71af4c407
Cory Forsyth <cory.forsyth@gmail.com>
2010-08-22 17:34:02 -0400

    LCD is now a class and we instantiate it instead of a module

--- 2/2.rb
+++ 2/2.rb
@@ -1,4 +1,4 @@
-module LCD
+class LCD
   
   NUMBERS = [
     [true,  true,  true, false, true,  true,  true],
@@ -15,55 +15,54 @@ module LCD
   
   DEFAULT_SIZE = 2
   
-  class << self
-    def print(string, opts={})
-      size = opts[:size] || DEFAULT_SIZE
-      
-      numbers = string.scan(/\d/).map {|i| i.to_i }
-
-      output = []
-      
-      
-      output << numbers.map {|number| row0(number, size)}.join(" ")
-      
-      size.times do
-        output << numbers.map{|number| row1(number, size)}.join(" ")
-      end
-      
-      output << numbers.map{|number| row2(number, size)}.join(" ")
-
-      size.times do
-        output << numbers.map{|number| row3(number, size)}.join(" ")
-      end
-      
-      output << numbers.map{|number| row4(number, size)}.join(" ")
-      
-      output.join("\n")
-    end
+  def initialize(string)
+    @numbers = string.scan(/\d/).map {|i| i.to_i }
+  end
   
-    def row0(number, size=DEFAULT_SIZE)
-      " " + (str_for_section(number, 0) * size) + " "
+  def print(size=DEFAULT_SIZE)
+    @size = size
+    
+    output = []
+    
+    output << @numbers.map {|number| row0(number)}.join(" ")
+    
+    @size.times do
+      output << @numbers.map{|number| row1(number)}.join(" ")
     end
     
-    def str_for_section(number, section)
-      string_for_section = [0,3,6].include?(section) ? "-" : "|"
-      NUMBERS[number][section] ? string_for_section : " "
+    output << @numbers.map{|number| row2(number)}.join(" ")
+
+    @size.times do
+      output << @numbers.map{|number| row3(number)}.join(" ")
     end
+    
+    output << @numbers.map{|number| row4(number)}.join(" ")
+    
+    output.join("\n")
+  end
+
+  def row0(number)
+    " " + (str_for_section(number, 0) * @size) + " "
+  end
   
-    def row1(number, size=DEFAULT_SIZE)
-      str_for_section(number, 1) + (" " * size) + str_for_section(number, 2)
-    end
-    
-    def row2(number, size=DEFAULT_SIZE)
-      " " + str_for_section(number, 3) * size + " "
-    end
-    
-    def row3(number, size=DEFAULT_SIZE)
-      str_for_section(number, 4) + (" " * size) + str_for_section(number, 5)
-    end
-    
-    def row4(number, size=DEFAULT_SIZE)
-      " " + str_for_section(number, 6) * size + " "
-    end
+  def str_for_section(number, section)
+    string_for_section = [0,3,6].include?(section) ? "-" : "|"
+    NUMBERS[number][section] ? string_for_section : " "
+  end
+
+  def row1(number)
+    str_for_section(number, 1) + (" " * @size) + str_for_section(number, 2)
+  end
+  
+  def row2(number)
+    " " + str_for_section(number, 3) * @size + " "
+  end
+  
+  def row3(number)
+    str_for_section(number, 4) + (" " * @size) + str_for_section(number, 5)
+  end
+  
+  def row4(number)
+    " " + str_for_section(number, 6) * @size + " "
   end
 end
