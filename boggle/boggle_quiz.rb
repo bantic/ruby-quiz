f9664bb0c269ed94a562229d47bba88fd7d5d839
Cory Forsyth <cory.forsyth@gmail.com>
2011-01-06 12:10:27 -0500

    got it going for major cardinal directions n,e,w,s. need to add diagonals now.

--- boggle/boggle_quiz.rb
+++ boggle/boggle_quiz.rb
@@ -1,79 +1,121 @@
 class Boggle
   DEFAULT_MAX_LENGTH = 10
+  DEFAULT_MIN_LENGTH = 3
+  
+  def self.word_dictionary_file
+    @@word_dictionary_file = "/usr/share/dict/words"
+  end
+  
+  def self.matches_word?(word_fragment)
+     !`grep -i -e "^#{word_fragment}" #{word_dictionary_file}`.empty?
+  end
+  
+  def self.word_exists?(full_word)
+    !`grep -i -e "^#{full_word}$" #{word_dictionary_file}`.empty?
+  end
+  
   def initialize(*val_array)
     @rows = *val_array
   end
   
   def [](col_idx, row_idx)
+    return nil if col_idx < 0 || row_idx < 0
     if @rows[row_idx]
       @rows[row_idx][col_idx]
     end
   end
   
   def letter_sequences(opts={})
-    max_length = opts[:max_length] || DEFAULT_MAX_LENGTH
-    
-    follow_path([[0,0]])
+    @max_length = opts[:max_length] || DEFAULT_MAX_LENGTH
+    @min_length = opts[:min_length] || DEFAULT_MIN_LENGTH
+    @visited_paths = []
+    @found_paths   = []
+    follow_path([[0,0]]).collect do |path|
+      path.collect {|tuple| self[*tuple]}
+    end
   end
+  
+  DIRECTIONS = ["E","S","W","N","NE", "SE", "NW", "SW"]
   
   private
   
-  def follow_path(path, paths=[])
-    return paths if path.empty?
-    right_paths = follow_path_right(path, paths)
+  def follow_path(path)
+    path = path.dup
+    return @found_paths if path.empty?
+    right_paths = follow_path_right(path)
     if right_paths.nil?
-      left_paths  = follow_path_left(path, paths)
-      if left_paths.nil?
-        down_paths  = follow_path_down(path, paths)
-        if down_paths.nil?
-          up_paths    = follow_path_up(path, paths)
+      puts "right paths nil, checking down<br>"
+      down_paths  = follow_path_down(path)
+      if down_paths.nil?
+        puts "down paths nil, checking left<br>"
+        left_paths  = follow_path_left(path)
+        if left_paths.nil?
+          puts "left paths nil, checking up<br>"
+          up_paths    = follow_path_up(path)
           if up_paths.nil?
+            puts "up paths nil, popping path and trying again<br>"
             path.pop
-            return follow_path(path, paths)
+            puts "path: #{path.inspect}. paths: #{@visited_paths.inspect}"
+            return follow_path(path)
           end
         end
       end
     end
   end
   
-  def follow_path_right(path, paths)
-    follow_path_dir(path, paths, "right")
+  def follow_path_right(path)
+    follow_path_dir(path, "E")
   end
 
-  def follow_path_up(path, paths)
-    follow_path_dir(path, paths, "up")
+  def follow_path_up(path)
+    follow_path_dir(path, "N")
   end
 
-  def follow_path_down(path, paths)
-    follow_path_dir(path, paths, "down")
+  def follow_path_down(path)
+    follow_path_dir(path, "S")
   end
 
-  def follow_path_left(path, paths)
-    follow_path_dir(path, paths, "left")
+  def follow_path_left(path)
+    follow_path_dir(path, "W")
   end
 
   
-  def follow_path_dir(path, paths, dir)
+  def follow_path_dir(path, dir)
+    return nil if path.size >= @max_length
+    path = path.dup
     col_idx, row_idx = path.last
     
+    current_tuple = [col_idx, row_idx]
+    
     next_tuple = case dir
-    when "up"
+    when "N"
       [ col_idx, row_idx - 1 ]
-    when "right"
+    when "E"
       [col_idx + 1, row_idx]
-    when "down"
+    when "S"
       [ col_idx, row_idx + 1 ]
-    when "left"
-      [ col_idx, row_idx + 1 ]
+    when "W"
+      [ col_idx - 1, row_idx ]
     end
     
-    puts "dir: #{dir}, next tuple: #{next_tuple.inspect}. letter? #{ self[*next_tuple]}. path includes? #{path.include?(next_tuple)}. paths: #{paths.inspect}<br />\n"
+    puts "dir: #{dir}. cur tuple: #{current_tuple.inspect}. next tuple: #{next_tuple.inspect}. letter? #{ self[*next_tuple]}. path: #{path.inspect}. path includes? #{path.include?(next_tuple)}. paths: #{@visited_paths.inspect}<br />\n"
     
     return unless self[ *next_tuple ]
     if !path.include?(next_tuple)
+      puts "path didn't include #{next_tuple.inspect}. current @visited_paths: #{@visited_paths.inspect}<br>"
       path << next_tuple
-      if !paths.include?(path)
-        follow_path(path, paths << path)
+      puts "new path: #{path.inspect}<br>"
+      puts "current @visited_paths: #{@visited_paths.inspect}<br>"
+      if !@visited_paths.include?(path)
+        if path.size >= @min_length
+          @found_paths << path
+        end
+        @visited_paths << path
+        puts "@visited_paths didn't include the new path. new @visited_paths: #{@visited_paths.inspect}. RETURNING!<Br>"
+        return follow_path(path)
+      else
+        puts "@visited_paths included new path. @visited_paths: #{@visited_paths.inspect}. new path: #{path.inspect}"
+        return nil
       end
     end
   end
--- boggle/boggle_spec.rb
+++ boggle/boggle_spec.rb
@@ -8,7 +8,7 @@ describe Boggle do
                     ["m","n","o","p"])
   end
 
-  it "should be addressable by [row, col]" do
+  it "should be addressable by [col, row]" do
     @b[0,0].should == "a"
     @b[1,0].should == "b"
     @b[0,1].should == "e"
@@ -18,11 +18,78 @@ describe Boggle do
     @b[4,4].should be_nil
   end
   
-  it "should be able to generate legal letter sequences" do
-    @letter_sequences = @b.letter_sequences(:max_length => 3)
-    @letter_sequences.should include?(["a","b","c"])
-    @letter_sequences.should include?(["a","b","f"])
-    @letter_sequences.should include?(["a","e","f"])
-    @letter_sequences.should include?(["a","e","i"])
+  it "should not allow negative row or col indexes" do
+    @b[-1,0].should be_nil
+    @b[0,-1].should be_nil
+  end
+  
+  context "word matching" do
+    it "should match starts words" do
+      Boggle.matches_word?("absten").should be_true
+      Boggle.matches_word?("absTEN").should be_true
+      Boggle.matches_word?("abstentsislk").should be_false
+    end
+    
+    it "should check for existence of words" do
+      Boggle.word_exists?("abstentious").should be_true
+      Boggle.word_exists?("absTENTious").should be_true
+      Boggle.word_exists?("absten").should be_false
+    end
+  end
+  
+  context "solving boards" do
+    before(:each) do
+      @b1 = Boggle.new( ["b","a"],
+                      ["h","t"])
+      @b2 = Boggle.new( ["b","a","r"],
+                        ["h","t","k"],
+                        [""])
+    end
+    it "should do something" do
+      
+    end
+  end
+  
+  context "letter sequences" do
+    before(:each) do
+      @b = Boggle.new( ["a","b"],
+                      ["e","f"])
+    end
+    it "should be able to generate legal letter sequences" do
+      letter_sequences = @b.letter_sequences(:min_length => 2)
+      letter_sequences.should include(["a","b"])
+      letter_sequences.should include(["a","b","f"])
+      letter_sequences.should include(["a","b","f","e"])
+      letter_sequences.should include(["a","e"])
+      letter_sequences.should include(["a","e","f"])
+      letter_sequences.should include(["a","e","f","b"])
+
+      # letter_sequences.should include(["a","b"])
+      # letter_sequences.should include(["a","b","f"])
+      # letter_sequences.should include(["a","f"])
+      # letter_sequences.should include(["a","f","b"])
+      # letter_sequences.should include(["a","f","b","e"])
+      # letter_sequences.should include(["a","b","f","e"])
+      # letter_sequences.should include(["a","e"])
+      # letter_sequences.should include(["a","e","b"])
+      # letter_sequences.should include(["a","e","b","f"])
+      # letter_sequences.should include(["a","e","f"])
+      # letter_sequences.should include(["a","e","f","b"])
+
+    end
+    
+    it "should accept min_length" do
+      letter_sequences = @b.letter_sequences(:min_length => 3)
+      letter_sequences.each do |sequence|
+        sequence.size.should >= 3
+      end
+    end
+    
+    it "should accept max length" do
+      letter_sequences = @b.letter_sequences(:max_length => 3)
+      letter_sequences.each do |sequence|
+        sequence.size.should <= 3
+      end
+    end
   end
 end
